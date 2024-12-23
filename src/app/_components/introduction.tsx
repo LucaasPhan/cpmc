a9c26c370613261bd78302c4f21f07806dba8d7d
Jimmy Tran <125101569+jimmytrn154@users.noreply.github.com>
2024-12-23 23:52:08 +0700

    Update introduction.tsx for alignment

--- src/app/_components/introduction.tsx
+++ src/app/_components/introduction.tsx
@@ -2,7 +2,7 @@ const Introduction = () => {
     return (
         <section id="#introduce">
             <div className="flex container items-center justify-start flex-col lg:flex-row pt-10 gap-10 px-8 text-white w-full">
-                <p>
+                <p className="text-center lg:text-left mx-auto">
                 Central Park Media Crew là một cộng đồng dành cho tất cả những ai đam mê nhiếp ảnh và truyền thông. Chúng tôi không chỉ đơn thuần là một câu lạc bộ, mà còn là một gia đình gắn kết bởi tình yêu chung dành cho hình ảnh và nghệ thuật.
                 </p>
             </div>
