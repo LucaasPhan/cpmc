d1d0751dd8b0a21c8de55bfb6a19fa63968cccec
Mitelite <nhathuyphan.1903@gmail.com>
2025-01-01 22:23:03 +0700

    repush

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -10,9 +10,6 @@ const Apply = () => {
             <div className="relative flex items-center justify-center h-[100vh] bg-center[-50] bg-no-repeat bg-[url('/banner.jpg')] bg-cover overflow-hidden bg-purple-950/50 bg-blend-multiply">
                 <div className="flex flex-col gap-7 order-1 sm:order-[0] items-center align-middle">
                     <h1 className="font-semibold text-white text-7xl pb-10"><span>Chúng mình mở tuyển rồi nè!!!</span></h1>
-                    {/* <Link href="/">
-                        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Về lại trang chủ</button>
-                    </Link> */}
                     <a 
                         href="https://docs.google.com/forms/d/e/1FAIpQLSfDIW59kvpC23zq3oQ2pnkQKfA3caksTXLCVaM2PKWTM0HbHQ/viewform?embedded=true"
                         title="Recruit"
