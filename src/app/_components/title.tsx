55beb710b41f3416bb2a305e314285082a634ad5
LucaasPhan <nhathuyphan.1903@gmail.com>
2025-02-18 11:32:21 +0700

    color & landing page tweak

--- src/app/_components/title.tsx
+++ src/app/_components/title.tsx
@@ -9,13 +9,13 @@ const raleway = Raleway({
 const Title = () => {
     return(
         <section id="#" className="">
-            <div className="relative flex items-end pb-16 justify-around flex-col sm:flex-row pt-16 bg-center[-50] bg-no-repeat bg-[url('/banner.jpg')] bg-cover h-[100vh] overflow-hidden bg-purple-950/25 bg-blend-multiply">
+            <div className="relative flex items-end pb-16 justify-end lg:justify-around flex-col pt-16 bg-center[-50] bg-no-repeat bg-[url('/banner.jpg')] bg-cover h-[100vh] overflow-hidden bg-[#202a44]/25 bg-blend-multiply">
                 <div className="w-[100vw] flex flex-col gap-y-5 px-[5%] xl:px-[10%] 2xl:px-[20%] items-left">
                     <div className="md:w-[60%]">
-                        <p className={`text-4xl md:text-5xl md:leading-[60px] ${raleway.className} drop-shadow-md`}>Ghi lại <span className="font-semibold">khoảnh khắc</span>, lan tỏa <span className="font-semibold">tiếng nói</span>, tạo nên <span className="font-semibold">sự khác biệt</span>.</p>
+                        <p className={`border text-4xl md:text-5xl md:leading-[60px] ${raleway.className} drop-shadow-md`}>Ghi lại <span className="font-semibold">khoảnh khắc</span>, lan tỏa <span className="font-semibold">tiếng nói</span>, tạo nên <span className="font-semibold">sự khác biệt</span>.</p>
                     </div>
                     <Link href="/about">
-                        <div className="text-white w-fit bg-blue-700 hover:scale-[1.05] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-7 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 transition-all">Tìm hiểu thêm →</div>
+                        <div className="text-white w-fit bg-[#917153] hover:scale-[1.05] hover:bg-[#7c6147] focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-7 py-2.5 focus:outline-none transition-all">Tìm hiểu thêm →</div>
                     </Link>
                 </div>
             </div>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -55,7 +55,7 @@ const About = () => {
 
     if (!isLoaded) {
         return (
-            <div className="relative flex items-center justify-around flex-col sm:flex-row pt-16 bg-center[-50] bg-no-repeat bg-[url('/banner.jpg')] bg-cover h-[100vh] overflow-hidden bg-purple-950/25 bg-blend-multiply">
+            <div className="relative flex items-center justify-around flex-col sm:flex-row pt-16 bg-center[-50] bg-no-repeat bg-[url('/banner.jpg')] bg-cover h-[100vh] overflow-hidden bg-[#202a44]/25 bg-blend-multiply">
                 <h1 className="font-semibold text-5xl lg:text-6xl super-lg:text-5xl text-center">Đang tải...</h1>
             </div>
         );
