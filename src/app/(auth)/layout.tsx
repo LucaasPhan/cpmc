87bc69b532623ee49adbba4100385064a11dc9fb
Phan Nhat Huy <89201985+LucaasPhan@users.noreply.github.com>
2025-02-20 02:57:31 +0700

    auth footer update

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
+import Footer from "@/components/footer";
 
 const AuthLayout = ({children}: {children: ReactNode}) => {
     return (
@@ -23,30 +22,7 @@ const AuthLayout = ({children}: {children: ReactNode}) => {
             <div className="flex flex-col justify-center items-center pt-16 pb-24">
                 <div>{children}</div>
             </div>
-            <div className="bg-[#010101] text-color justify-around block w-full">
-                <div className="flex flex-col md:flex-row items-center justify-between py-2 gap-3 px-[25vw]">
-                    <div className="flex justify-center items-center py-2 order-1 md:order-0 text-sm whitespace-nowrap">
-                    <p>© 2024 Central Park Media Crew</p>
-                    </div>
-                    <div className="flex items-center justify-center order-0 md:order-1 flex-shrink-0">
-                    <a 
-                        href="https://instagram.com/cpmc.vcp/"
-                        title="Instagram"
-                        target="_blank"
-                        rel="noopener"
-                        className="mr-1">
-                        <FaInstagram className="color"></FaInstagram>
-                    </a>
-                    <a 
-                        href="https://facebook.com/cpmc.vcp"
-                        title="Instagram"
-                        target="_blank"
-                        rel="noopener">
-                        <FaFacebook className="color"></FaFacebook>
-                    </a>
-                    </div>
-                </div>
-            </div>
+            <Footer/>
         </body>
     );
 };
